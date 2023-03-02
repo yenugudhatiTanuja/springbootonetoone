fde8dd8f83f61199bdde518d4688b9d0c5fa3629
yenugudhatiTanuja <107547575+yenugudhatiTanuja@users.noreply.github.com>
2023-03-02 15:49:02 +0530

    Add files via upload

---
+++ springbootone-one/src/main/java/com/example/demo/SpringbootoneOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootoneOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootoneOneApplication.class, args);
+	}
+
+}
